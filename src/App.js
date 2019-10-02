e6954eaa26bae5c176b7bb5dfa1fa37d45388d59
AthiraPKumar <athirapkumar16@gmail.com>
2019-10-02 19:52:20 +0530

    Pushing rect app-complete guide udemy by Maximilliam S

--- src/App.js
+++ src/App.js
@@ -1,26 +1,150 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+// import Radium, {StyleRoot} from 'radium';
+import Person from './Person/Person';
+import Validation from './ValidationComponent/ValidationComponent';
+import Char from './Char/Char';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    persons: [
+      {
+        id:'xyz',
+        name: 'Max', 
+        age: '28'
+      },
+      {
+        id:'abc',
+        name: 'Athira', 
+        age: '25'
+      },
+      {
+        id:'uvw',
+        name: 'Pia', 
+        age: '39'
+      }
+    ],
+    showPersons:false,
+    userInput: ''
+  }
+
+  switchHandler = (Newname) => {
+    this.setState({
+      persons: [ {
+        name: Newname, 
+        age: '25'
+      },
+      {
+        name: 'Athira', 
+        age: '25'
+      },
+      {
+        name: 'Piyali', 
+        age: '39'
+      }
+    ]
+    })
+    console.log('handler clicked');
+  }
+
+  inputChangeHandler = (event) => {
+    this.setState({userInput : event.target.value});
+  }
+
+
+  deleteCharHandler = (index) => {
+    let text = this.state.userInput.split('');  // since the userinput is empty use split.
+    text.splice(index, 1);
+    let updatedText = text.join('');
+    this.setState({userInput : updatedText});
+  }
+
+  namechangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    // we get an event object here
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    person.name = event.target.value;
+
+    const persons2 = [...this.state.persons];
+    persons2[personIndex] = person;
+    this.setState({persons: persons2});
+  }
+
+  togglePersonsHandler = () => {
+    let doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  deletePersonHandler = (index) => {
+    // const persons1 = this.state.persons.slice();   -- an alternative for the spread operator
+    // always use a spread operator so that the original value in the array is not lost.
+
+    const persons1 = [...this.state.persons];
+    persons1.splice(index,1);
+    this.setState({persons: persons1})
+  }
+
+  render() {
+    const charList = this.state.userInput.split('').map((ch,index) => {
+      return <Char character={ch} key={index} click={() => this.deleteCharHandler(index)}/>;
+    });
+
+    const inputStyle = {
+      backgroundColor:'red',
+      color:'#fff',
+      font:'inherit',
+      border: '1px solid blue',
+      padding: '8px'
+    }
+
+    let persons1 = null;
+
+    if(this.state.showPersons){
+      persons1 = (
+      <div>
+        {this.state.persons.map((person,index) => {
+         return <Person 
+         key={person.id} 
+         name={person.name} 
+         age={person.age} 
+         changed={(event) => this.namechangeHandler(event, person.id)} 
+         click={() => this.deletePersonHandler(index)}/>
+        })}
+      </div>
+      );
+
+      inputStyle.backgroundColor='green';
+ 
+    }
+
+    let classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('orange');     // classes=['red'];
+    }if(this.state.persons.length <= 1){
+      classes.push('bold');       // classes=['red', 'bold'];
+    }
+
+    return (
+      // <StyleRoot>
+      <div className="App">
+        <h1>Hello World</h1>
+        <p className={classes.join(' ')}>Its working</p>
+        <button style={inputStyle} onClick={this.togglePersonsHandler}>Switch</button>
+          {persons1}
+        <br/>
+          <input type="text" onChange={this.inputChangeHandler} value={this.state.userInput}/>
+          <p>{this.state.userInput}</p>
+          <Validation inputLength={this.state.userInput.length}/>
+          {charList}
+      </div>  
+      // </StyleRoot>  
+    );
+  }
 }
 
 export default App;
